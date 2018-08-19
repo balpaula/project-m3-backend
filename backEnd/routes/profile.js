d8cc3017eb43d30304ff46361bad554717450f48
Paula Balcells <paulabalcells@gmail.com>
2018-08-19 19:41:20 +0200

    descriptions done

--- backEnd/routes/profile.js
+++ backEnd/routes/profile.js
@@ -3,6 +3,20 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+router.post('/description', (req, res, next) => {
+    const user = req.session.currentUser;
+    const description = req.body.description;
+
+    User.findByIdAndUpdate(user._id, {description})
+        .then(user => {
+            user.save()
+                .then(() => {
+                    res.json(user.description)
+                })
+        })
+        .catch(next);
+})
+
 router.get('/:username', (req, res, next) => {
     const { username } = req.params;
     User.findOne({username})
@@ -10,7 +24,7 @@ router.get('/:username', (req, res, next) => {
         .then((user) => {
             res.json(user);
         })
-        .catch(next)
+        .catch(next);
 })
 
 module.exports = router;
