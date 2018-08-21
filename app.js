75844ebb01b73b635fd9c00c9adcc9d89e4c0c00
Paula Balcells <paulabalcells@gmail.com>
2018-08-21 16:19:05 +0200

    change name var

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 // }));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_URL);
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
