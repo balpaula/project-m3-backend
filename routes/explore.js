354d69b1d4f52b4b60d6a136603416e573dc68ad
Paula Balcells <paulabalcells@gmail.com>
2018-08-22 10:23:40 +0200

    elastic search

--- routes/explore.js
+++ routes/explore.js
@@ -5,11 +5,29 @@ const Trip = require('../models/trip');
 
 router.get('/', (req, res, next) => {
     const user = req.session.currentUser;
-    Trip.aggregate( [{ $sample: { size: 12 } }] )
+    Trip.aggregate( [{ $sample: { size: 12 } }] ) 
         .then((randomTrips) => {
             res.json(randomTrips);
         })
-        .catch(next)
+        .catch(next);
+})
+
+router.get('/search/:text', (req, res, next) => {
+    let { text } = req.params;
+    let queryRegex = new RegExp(text, 'i');
+    Trip.find()
+        .populate('owner')
+        .populate('places')
+        .then(trips => {
+        Trip.find({ name: queryRegex })
+                .then(results => {
+                    console.log(results)
+                    res.json(results);
+                })
+        })
+        .catch(error => {
+            console.log(error)
+        });
 })
 
 module.exports = router;
